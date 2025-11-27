0ae48981838cb3960d3f64052c9e726aab226381
Julek <julekjulas@gmail.com>
2025-11-27 11:04:17 +0100

    More TODO's

--- TODO.cs
+++ TODO.cs
@@ -13,8 +13,10 @@ For now
 the whole day is wonked, gotta fix that
 -Add more settings
 -As for vanilla needs they work but modded might use a different timer for whatever reason, remember that
-
-
+-Fix the text in the mod config menu
+-Make settings look better
+-Our todays sponsor is Raid Shadow Legends
+-Maybe fix the date completly breaking if you change the time multiplier mid-game though note: it may be impossible and if possible very hard
 
 
 
