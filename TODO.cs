e7dc6191e1a402f1b1dfe70842ff070ece6d6bd9
Julek <julekjulas@gmail.com>
2025-11-27 10:51:31 +0100

    Added TODO

---
+++ TODO.cs
@@ -0,0 +1,36 @@
+﻿/*
+Not exactly sure why I made the TODO file a CS file but eh
+
+RIGHT
+
+We add or remove TODOs here
+
+For now
+
+-Fix the blood loss issue
+-Make sure mod compatibility is solid
+-Already spotted an issue with this https://steamcommunity.com/sharedfiles/filedetails/?id=3520836521&searchtext=weather
+the whole day is wonked, gotta fix that
+-Add more settings
+-As for vanilla needs they work but modded might use a different timer for whatever reason, remember that
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
