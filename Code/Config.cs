db86ecb3d5cdf0bff53ee6babd42b60935a73486
Julek <julekjulas@gmail.com>
2026-04-11 11:23:30 +0200

    Greatly Improved the settings page

--- Code/Config.cs
+++ Code/Config.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -10,14 +11,20 @@ namespace DayStretch
 {
     public class DayStretch : ModSettings
     {
-
+        public float FakeTimeMultiplier = 1f;
         public float TimeMultiplier = 1f;
-        public bool WorkRelated = true;
+        public float FakeWorkMultiplier = 1f;
+        public float WorkMultiplier = 1f;
+        public bool ShouldWorkFollow = true;
+        public bool firstPopup = false;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref TimeMultiplier, "TimeMultiplier", 1f);
-            Scribe_Values.Look(ref WorkRelated, "WorkRelated", true);
+            Scribe_Values.Look(ref FakeTimeMultiplier, "FakeTimeMultiplier", 1f); // for the apply button
+            Scribe_Values.Look(ref WorkMultiplier, "WorkRelated", 1f);
+            Scribe_Values.Look(ref FakeWorkMultiplier, "FakeWorkRelated", 1f); // for the apply button
+            Scribe_Values.Look(ref ShouldWorkFollow, "ShouldWorkFollow", true);
             base.ExposeData();
         }
     }
@@ -32,15 +39,42 @@ namespace DayStretch
         }
         public override void DoSettingsWindowContents(Rect inRect)
         {
-
+            settings.firstPopup = false;
             Listing_Standard listingStandard = new Listing_Standard();
             listingStandard.Begin(inRect);
-            listingStandard.CheckboxLabeled("Should work be scaled ", ref settings.WorkRelated, "If ticked on, work will be x times slower or faster");
-            listingStandard.Label("Time Multiplier: " + settings.TimeMultiplier.ToString("0.0"));
-            settings.TimeMultiplier = listingStandard.Slider(settings.TimeMultiplier, 0.1f, 20f);
-            settings.TimeMultiplier = (float)Math.Round(settings.TimeMultiplier, 1);
-            listingStandard.Label("The game MUST be restarted every time the time multiplier is changed or else the code cannot update.");
-            listingStandard.Gap();
+            listingStandard.Label("Time Multiplier: " + settings.FakeTimeMultiplier.ToString("0.0"));
+            settings.FakeTimeMultiplier = listingStandard.Slider(settings.FakeTimeMultiplier, 0.1f, 20f);
+            settings.FakeTimeMultiplier = (float)Math.Round(settings.FakeTimeMultiplier, 1);
+            listingStandard.Label($"Current Time Multiplier: {settings.TimeMultiplier}");
+            listingStandard.CheckboxLabeled("Should work use its own slider ", ref settings.ShouldWorkFollow);
+            if (settings.ShouldWorkFollow)
+            {
+                listingStandard.Label("Work Related Multiplier: " + settings.FakeWorkMultiplier.ToString("0.0"));
+                settings.FakeWorkMultiplier = listingStandard.Slider(settings.FakeWorkMultiplier, 0.1f, 20f);
+                settings.FakeWorkMultiplier = (float)Math.Round(settings.FakeWorkMultiplier, 1);
+                listingStandard.Label($"Current Work Related Multiplier: {settings.WorkMultiplier}"); // hello everybody my name is Multiplier
+            }
+            else
+            {
+                settings.FakeWorkMultiplier = settings.TimeMultiplier;
+                settings.WorkMultiplier = settings.TimeMultiplier;
+            }
+            if (listingStandard.ButtonText("Apply"))
+            {
+                settings.TimeMultiplier = settings.FakeTimeMultiplier;
+                settings.WorkMultiplier = settings.FakeWorkMultiplier;
+                if (!settings.ShouldWorkFollow)
+                {
+                    settings.WorkMultiplier = settings.TimeMultiplier;
+                    Find.WindowStack.Add(new Dialog_MessageBox($"Game has to be restarted for the settings to load, new multiplier: {settings.FakeTimeMultiplier}"));
+                }
+                else
+                {
+                    Find.WindowStack.Add(new Dialog_MessageBox($"Game has to be restarted for the settings to load\nNew multipliers: (Global){settings.FakeTimeMultiplier}, (Work){settings.FakeWorkMultiplier}"));
+                }
+
+
+            }
             listingStandard.End();
             base.DoSettingsWindowContents(inRect);
         }
--- Code/StatPart.cs
+++ Code/StatPart.cs
@@ -33,5 +33,18 @@ namespace DayStretch
             return $"DayStretch multiplier: x{Settings.Instance.TimeMultiplier}";
         }
     }
+
+    public class StatPart_WorkDayStretchMultiplier : StatPart
+    {
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            val /= Settings.Instance.WorkMultiplier;
+        }
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            return $"DayStretch multiplier: /{Settings.Instance.WorkMultiplier}";
+        }
+    }
 }
 
