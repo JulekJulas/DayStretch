cec2a05eb1865d8b4c661d4729c717684af88c47
Julek <julekjulas@gmail.com>
2026-06-04 01:44:34 +0200

    Fixed syntax in Mod patches and converted a patch into the Advanced Patcher format

--- Code/NonAutomatablePatches.cs
+++ Code/NonAutomatablePatches.cs
@@ -158,27 +158,6 @@ namespace DayStretch
 
         
 
-        [HarmonyPatch(typeof(Hediff))]
-        [HarmonyPatch("TickInterval")]
-        static IEnumerable<CodeInstruction> IHateHediffs(IEnumerable<CodeInstruction> instructions)
-        {
-            float[] whyAreThereSoManyNumbers = { 60f, 60000f, 600f, 400f, 200f };
-            foreach (var instr in instructions)
-            {
-                if (instr.opcode == OpCodes.Ldc_R4 && instr.operand is float val)
-                {
-                    foreach (float target in whyAreThereSoManyNumbers)
-                    {
-                        if (Mathf.Approximately(val, target))
-                        {
-                            instr.operand = target * Settings.Instance.TimeMultiplier;
-                            break;
-                        }
-                    }// my system can only handle 3 at most, why 5 ludeon?
-                }
-                yield return instr;
-            }
-        }
 
 
 
